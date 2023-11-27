23587867a8a98ef0a6ef89971a10af3060ec46ad
SayyedFiroz <119703925+SayyedFiroz@users.noreply.github.com>
2023-11-27 07:33:12 +0530

    SHM

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -137,11 +137,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS =os.path.join(BASE_DIR,'static'),
 STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles_build','static')
-MEDIA_ROOT=os.path.join(BASE_DIR,'static')
-MEDIA_URL = '/Media/'
+
 
 
 LOGIN_REDIRECT_URL='/afterlogin'
