dd10153717304bb7f5e6c690b523c7bdb189d388
SayyedFiroz <119703925+SayyedFiroz@users.noreply.github.com>
2023-11-28 05:49:38 +0530

    deploying on railway app

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = 'hpbv()ep00boce&o0w7z1h)st148(*m@6@-rk$nn)(n9ojj4c0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+ALLOWED_HOSTS = ["*"]
 
-
-ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
+# ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
 
 # Application definition
 
@@ -98,7 +98,6 @@ DATABASES = {
     }
 }
 
-# ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
 
 
 # Password validation
@@ -137,10 +136,11 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS =[os.path.join(BASE_DIR, "static"),]
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'static')
 
