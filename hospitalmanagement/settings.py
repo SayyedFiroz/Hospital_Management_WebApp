43771f5a399b5a5f0450728b0a7396995732366a
SayyedFiroz <119703925+SayyedFiroz@users.noreply.github.com>
2023-11-29 02:36:44 +0530

    retrying on vercel

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -140,7 +140,7 @@ USE_TZ = False
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS =[os.path.join(BASE_DIR, "static"),]
 
